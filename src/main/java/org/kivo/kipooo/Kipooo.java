2073b51b02dc5ac24c6771d56d23ecc946bc6a9d
Iverins <iverins_@outlook.com>
2022-02-25 18:38:47 +0800

    Initial commit

---
+++ src/main/java/org/kivo/kipooo/Kipooo.java
@@ -0,0 +1,28 @@
+package org.kivo.kipooo;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Kipooo extends JavaPlugin {
+
+    public static Kipooo INSTANCE;
+
+    {
+        INSTANCE = this;
+    }
+
+    @Override
+    public void onLoad() {
+
+    }
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
